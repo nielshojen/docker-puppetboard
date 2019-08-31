7a07f7cbfb05204f343906e36ea5b06309b7c654
Niels Højen <niels.hojen@bestseller.com>
2019-08-31 14:27:45 +0200

    Adding some info for the run

--- run.sh
+++ run.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
+/bin/echo "Starting Puppetboard ..."
 gunicorn -b 0.0.0.0:8000 puppetboard.app:app &
 
 if [[ ${ADMIN_USER} ]] && [[ ${ADMIN_PASS} ]] && [[ ! ${OAUTH2_PROXY_COOKIE_SECRET} ]]; then
+    /bin/echo "Generating htpasswd file from env ..."
     /usr/bin/htpasswd -b -c /etc/nginx/htpasswd/puppetboard ${ADMIN_USER} ${ADMIN_PASS}
+    /bin/echo "Starting nginx ..."
     /usr/sbin/nginx
     /usr/bin/tail -F /var/log/nginx/access.log
 elif [[ -f "/etc/nginx/htpasswd/puppetboard" ]]; then
+    /bin/echo "Found htpasswd file ..."
+    /bin/echo "Starting nginx ..."
     /usr/sbin/nginx
     /usr/bin/tail -F /var/log/nginx/access.log
 fi
 
 if [[ -f "/etc/oauth2_proxy.cfg" ]]; then
+    /bin/echo "Found oauth2 proxy config file ..."
+    /bin/echo "Starting oauth2_proxy ..."
     /usr/local/bin/oauth2_proxy
 elif [[ ${OAUTH2_PROXY_COOKIE_SECRET} ]] && [[ ${OAUTH2_PROXY_EMAIL_DOMAINS} ]] && [[ ${OAUTH2_PROXY_CLIENT_SECRET} ]]; then
+    /bin/echo "Starting oauth2_proxy with settings from env ..."
     /usr/local/bin/oauth2_proxy
 fi

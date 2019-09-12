e253a88b54a6a2162bdfb2a5b3e797ecba59ca16
Niels Højen <niels.hojen@bestseller.com>
2019-09-12 15:40:33 +0200

    Adding upstram

--- run.sh
+++ run.sh
@@ -17,10 +17,10 @@ fi
 if [[ -f "/etc/oauth2_proxy.cfg" ]]; then
     /bin/echo "Found oauth2 proxy config file ..."
     /bin/echo "Starting oauth2_proxy ..."
-    /usr/local/bin/oauth2_proxy
+    /usr/local/bin/oauth2_proxy --upstream=http://0.0.0.0:8000
 elif [[ ${OAUTH2_PROXY_COOKIE_SECRET} ]] && [[ ${OAUTH2_PROXY_EMAIL_DOMAINS} ]] && [[ ${OAUTH2_PROXY_CLIENT_SECRET} ]]; then
     /bin/echo "Starting oauth2_proxy with settings from env ..."
-    /usr/local/bin/oauth2_proxy
+    /usr/local/bin/oauth2_proxy --upstream=http://0.0.0.0:8000
 fi
 
 tail -f /dev/null
